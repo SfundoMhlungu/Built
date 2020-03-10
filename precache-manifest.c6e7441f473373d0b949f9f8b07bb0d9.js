c21d500d325abfe63a1b12105e604fed08118a39
Sfundo Mhlungu <mhlungusk@gmail.com>
2020-03-10 20:46:45 +0200

    pomodoro

---
+++ precache-manifest.c6e7441f473373d0b949f9f8b07bb0d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed1e74dc0ef9f32ac78c9c5207e1beb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05472c4d690131af7b6",
+    "url": "/static/css/main.725b2dad.chunk.css"
+  },
+  {
+    "revision": "7c43f04557d8947b5aac",
+    "url": "/static/js/2.8c28f7c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8c28f7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05472c4d690131af7b6",
+    "url": "/static/js/main.5646c688.chunk.js"
+  },
+  {
+    "revision": "2c8039270614a39c8cea",
+    "url": "/static/js/runtime-main.44716643.js"
+  }
+]);
